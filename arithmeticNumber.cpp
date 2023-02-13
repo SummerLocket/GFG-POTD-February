8c1bd36b1a37d39e1dff1bfe6a2b7b58c8bd71e7
Mahaveer SIngh Gurjar <125082310+codewithgfg@users.noreply.github.com>
2023-02-13 07:37:59 +0530

    Add files via upload

---
+++ arithmeticNumber.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        // code here
+      
+        if(C==0 && A==B){
+            return 1;
+        }
+        else if(C==0){
+            return 0;
+        }
+        
+        if(C<0){
+            if(A<B){
+                return 0;
+            }
+        }
+        else {
+            if(A>B){
+                return 0;
+            }
+        }
+        
+        if((B-A)%C == 0){
+            return 1;
+        }
+        return 0;
+    }
+};
